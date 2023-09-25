4ec310e5d466b531554493448c5f3d0212106cbe
amani esseili <esseiliamani@hotmail.com>
2023-09-25 20:51:08 +0200

    partials.. header and footer

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>comics laravel</title>
+    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
+</head>
+
+<body>
+    <header></header>
+    <main>
+        {{-- @yield(comicsContent) --}}
+
+    </main>
+    <footer></footer>
+
+</body>
+
+</html>
